ac12b40f4ff630b4d746b4cb7b5f452b92d90fb1
Uday Rao <udaykrao@Uday-Travel-MacBook-Pro.local>
2019-12-29 12:47:12 -0600

    Updates

---
+++ precache-manifest.6e92fb664ee5d418d15a86a7012698b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dff13299a3c0fef77ce71cea1cacd2",
+    "url": "/kanglish/index.html"
+  },
+  {
+    "revision": "af2623ee88a7684f2176",
+    "url": "/kanglish/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16cdcb34e6c0ed2f7a04",
+    "url": "/kanglish/static/js/2.e1bc746a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kanglish/static/js/2.e1bc746a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af2623ee88a7684f2176",
+    "url": "/kanglish/static/js/main.9aaf9024.chunk.js"
+  },
+  {
+    "revision": "765ed246f32c7aae32d0",
+    "url": "/kanglish/static/js/runtime-main.e6fee3eb.js"
+  }
+]);
